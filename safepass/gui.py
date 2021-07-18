353beb5abf935e7ab774171b5d1417da563dc6c2
dmdhrumilmistry <dhrumilmistry2015@gmail.com>
2021-07-18 15:13:41 +0530

    upload v1.0.3

--- safepass/gui.py
+++ safepass/gui.py
@@ -3,9 +3,9 @@ import random, os, string, pyperclip
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QStackedWidget, QLineEdit, QMessageBox
 from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon
 from safepass.user import User
-from safepass.paths import login_ui, create_acc_ui, pass_table_ui, save_info_ui
-
+from safepass.paths import login_ui, create_acc_ui, pass_table_ui, save_info_ui, icon
 
 user = 'AUTH_ME'
 
@@ -171,6 +171,7 @@ def message_box(box_type='warning',title='Title',text='information box', info_te
 
 
 SafePassApp = QApplication(argv)
+SafePassApp.setWindowIcon(QIcon(icon))
 SafePassLoginWindow = Login()
 widget = QStackedWidget()
 widget.setWindowTitle('SafePass')
--- safepass/paths.py
+++ safepass/paths.py
@@ -10,6 +10,7 @@ login_ui = ''
 create_acc_ui = ''
 pass_table_ui = ''
 save_info_ui = ''
+icon = ''
 
 paths = sys.path
 for path in paths:
@@ -17,7 +18,8 @@ for path in paths:
         log_file = join(path,'safepass','logs','SafePass.log')
         PASSWORD_DB = join(path,'safepass','db','passwords.db')
         USER_DB = join(path,'safepass','db','users.db')
-        login_ui = join(path,'safepass','ui','login.ui')
-        create_acc_ui = join(path,'safepass','ui','CreateAccount.ui')
-        pass_table_ui = join(path,'safepass','ui','PasswordsTable.ui')
-        save_info_ui = join(path,'safepass','ui','SaveInfo.ui')
+        login_ui = join(path,'safepass','UI','login.ui')
+        create_acc_ui = join(path,'safepass','UI','CreateAccount.ui')
+        pass_table_ui = join(path,'safepass','UI','PasswordsTable.ui')
+        save_info_ui = join(path,'safepass','UI','SaveInfo.ui')
+        icon = join(path, 'safepass','images','safepass.png')
